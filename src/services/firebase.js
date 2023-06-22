5d1423db204bdca408e17915a441773b492d8ff1
juniordtf <juniordtf@gmail.com>
2023-06-22 11:31:56 -0300

    Added firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAbdloFpDPlimbN5Nk4BKDwC3cJMtCcaNM",
+  authDomain: "integradorccee.firebaseapp.com",
+  projectId: "integradorccee",
+  storageBucket: "integradorccee.appspot.com",
+  messagingSenderId: "464464899919",
+  appId: "1:464464899919:web:c95b6ed18127a52e4e1f3a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
